c930a5717ee65350deba30fb88e62c6fee42f15a
Eduardo Figueiredo Basso <edu_fabric@outlook.com>
2025-06-21 16:35:22 -0300

    Sistema carregando Profissionais Cadastrados

--- backend/apps/register/urls.py
+++ backend/apps/register/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ClientViewSet, ProfessionalViewSet
 
 router = DefaultRouter()
-router.register(r'clients', ClientViewSet)
+router.register(r'clients', ClientViewSet, basename='client')
 router.register(r'professionals', ProfessionalViewSet)
 
 urlpatterns = [
